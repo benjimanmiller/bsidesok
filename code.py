bc614bbbe507e46176b46f8bc87858034aeaf8ff
Ben Miller <110997419+benjimanmiller@users.noreply.github.com>
2025-03-07 15:50:35 -0600

    added alot of comments and did some slight re-arrangements of the code.

--- code.py
+++ code.py
@@ -7,7 +7,6 @@ import random
 # Constants
 TORNADO_PIXELS = 40
 RED = (255, 0, 0)
-BLUE = (0, 0, 255)
 
 # Enable the LED Power (Pin 25)
 pin_led_en = digitalio.DigitalInOut(board.GP25)
@@ -24,16 +23,13 @@ pin_led = digitalio.DigitalInOut(board.GP13)
 pin_led.direction = digitalio.Direction.OUTPUT
 
 # GPIO Buttons
-# Re-enable btn_1 for detection
-btn_1 = digitalio.DigitalInOut(board.GP1)
-btn_1.direction = digitalio.Direction.INPUT
-btn_1.pull = digitalio.Pull.DOWN
-
-# GPIO Buttons
-# Re-enable btn_0 for detection
 btn_0 = digitalio.DigitalInOut(board.GP0)
 btn_0.direction = digitalio.Direction.INPUT
 btn_0.pull = digitalio.Pull.DOWN
+
+btn_1 = digitalio.DigitalInOut(board.GP1)
+btn_1.direction = digitalio.Direction.INPUT
+btn_1.pull = digitalio.Pull.DOWN
 
 
 def clear_leds():
@@ -44,7 +40,10 @@ def clear_leds():
 
 
 def blink(message_toggle, red_toggle):
-    # Picks a random LED and blinks it
+    # Blinks the LEDSs. 
+    # If message_toggle is True and if so activates message mode.
+    # If red_toggle is True all LEDs are red. 
+    # Otherwise, clear all leds and blink a random LED a random color. 
 
     # Checks to see if Message is toggled on.
     if message_toggle == True:
@@ -74,6 +73,8 @@ def set_all_red():
 
 
 def text_to_morse(text):
+    # Takes an input of text and then outputs it as an array encoded as morse code notated by .s and -s.
+    
     morse_dict = {
         "A": ".-",
         "B": "-...",
@@ -143,26 +144,34 @@ def text_to_morse(text):
 
 
 def message_mode():
-    # Toggles on the Message blinking
-    input_text = "benjimanmiller.com"
-    morse_code = text_to_morse(input_text)
+    # Takes the message text and converts it to morse code. 
+    # Two random RGB values are assigned to represent . and -. 
+    # The morse code is then transmitted to the world via the LEDs. 
+     
+    message_text = "benjimanmiller.com"
+    morse_code = text_to_morse(message_text)
 
+    # Set dash color
     dash_color = (
         random.randint(0, 255),
         random.randint(0, 255),
         random.randint(0, 255),
     )
-    
+
+    # Set dot color 
     dot_color = (
         random.randint(0, 255), 
         random.randint(0, 255), 
         random.randint(0, 255)
     )
 
-    print(f"Input message to be displayed in Morse Code: {input_text}")
-    
+    print(f"Input message to be displayed in Morse Code: {message_text}")
+
+    #Iterates of the digits in the morse code array and blinks them accordingly.   
     for digit in morse_code:
-        if digit == "-":
+
+        #If the current character is a - use one color and blink longer. 
+        if digit == "-": 
             print(f"Blinking Pixels: {digit}, Color: {dash_color}")
             for i in range(TORNADO_PIXELS):
                 pixels_DIO_Tornado[i] = dash_color
@@ -170,6 +179,8 @@ def message_mode():
             time.sleep(0.8)
             clear_leds()
             time.sleep(0.25)
+
+        #If the current character is a . use the other color and blink fater.
         if digit == ".":
             print(f"Blinking Pixels: {digit}, Color: {dot_color}")
             for i in range(TORNADO_PIXELS):
@@ -178,6 +189,8 @@ def message_mode():
             time.sleep(0.4)
             clear_leds()
             time.sleep(0.25)
+
+        #if the current chracter is an X then the LEDs are cleared, to indicate a space between words. 
         if digit == "X":
             print(" ")
             clear_leds()
@@ -196,23 +209,22 @@ def main():
     message_toggle = False
 
     while True:
-        # If button 1 is pressed, keep everything red
-        if btn_1.value:
-            if red_toggle == True:
-                red_toggle = False
-            else:
-                red_toggle = True
-            print(f"btn_1: Pressed, Red Toggle: {red_toggle}")
-            time.sleep(1)
-        if btn_0.value:
+        if btn_0.value: # If BTN_0 is pressed, toggel message mode on. 
             if message_toggle == True:
                 message_toggle = False
             else:
                 message_toggle = True
             print(f"btn_0: Pressed, Message Toggle: {message_toggle}")
             time.sleep(1)
+        if btn_1.value: # If BTN_1 is pressed, toggle red mode on.
+            if red_toggle == True:
+                red_toggle = False
+            else:
+                red_toggle = True
+            print(f"btn_1: Pressed, Red Toggle: {red_toggle}")
+            time.sleep(1)
         else:
-            # Otherwise, run the random blink pattern
+            # Runs the blink function, passing in the current message and red toggle states. 
             blink(message_toggle, red_toggle)
             time.sleep(0.1)
 
